bec1e9ffd0bf71897ebe0a7fb89ad8c5efe034b8
Christiano0407 <placebo0704@gmail.com>
2022-04-13 10:08:00 -0500

    carousel part 01

---
+++ carousel/appCarousel.js
@@ -0,0 +1,5 @@
+//** === Variables ==== */
+const row = document.querySelector(".row");
+const header = document.querySelector("#headerID");
+const progressBar = document.querySelector(".progress__bar");
+const container = document.querySelector(".container");
