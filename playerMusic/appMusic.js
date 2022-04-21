43bb5fb09b46c2be8ee399cfff2f946d2cc31c2f
Christiano0407 <placebo0704@gmail.com>
2022-04-21 09:47:12 -0500

    play part 02

--- playerMusic/appMusic.js
+++ playerMusic/appMusic.js
@@ -1,1 +1,9 @@
 //** === Player Audio === */
+//** >Variable < */
+const musicContainer = document.querySelector("#musicContainer");
+const progressContainer = document.querySelector("#progressContainer");
+const progress = document.querySelector("#progress");
+//** >  Buttons =  */
+const btnPrev = document.querySelector("#prev");
+const btnPlay = document.querySelector("#play");
+const btnNext = document.querySelector("#next");
