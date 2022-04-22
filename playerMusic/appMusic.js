43b64ab025d8c07d2600fadbe787f27a589fda73
Christiano0407 <placebo0704@gmail.com>
2022-04-22 09:07:12 -0500

    songs 02

--- playerMusic/appMusic.js
+++ playerMusic/appMusic.js
@@ -3,7 +3,14 @@
 const musicContainer = document.querySelector("#musicContainer");
 const progressContainer = document.querySelector("#progressContainer");
 const progress = document.querySelector("#progress");
+const title = document.querySelector("#title");
+const cover = document.querySelector("#cover"); //> IMG>
+
 //** >  Buttons =  */
 const btnPrev = document.querySelector("#prev");
 const btnPlay = document.querySelector("#play");
 const btnNext = document.querySelector("#next");
+//** Audio */
+const audio = document.querySelector("#audio");
+
+//** == Array Songs Titles == */
