5499da003faa930e5dff214a4434f4640f52fad5
Chris_Velázquez04 <placebo0704@gmail.com>
2022-07-27 14:30:18 +0000

    project One Javascript

--- exercises/exerciseApp.js
+++ exercises/exerciseApp.js
@@ -1,1 +1,10 @@
 //** == Pomodoro ==  */
+console.log("Hello World Pomodoro");
+const app = document.querySelector("#app");
+console.log(app);
+
+const task = []; // Tareas agregadas =
+let time = 0;
+const timer = null; // Time of init =
+const timeBreak = null; // Time of break => Descanso =
+const current = null; // Tiempo Transcurriendo para trabajo =
