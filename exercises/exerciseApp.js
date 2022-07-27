0ea1e0f64a5224c485b60ad9ecace6d2d86499c0
Chris_Velázquez04 <placebo0704@gmail.com>
2022-07-27 15:00:31 +0000

    part 01

--- exercises/exerciseApp.js
+++ exercises/exerciseApp.js
@@ -1,10 +1,26 @@
 //** == Pomodoro ==  */
 console.log("Hello World Pomodoro");
 const app = document.querySelector("#app");
-console.log(app);
+const timeAPP = document.querySelector(`#time`);
+const taskContainer = document.querySelector(`.task-container`);
+const idTask = document.querySelector(`#idText`);
+const badSubmit = document.querySelector(`#bADDSub`);
+const form = document.querySelector(`#form`);
+//console.log(form);
 
+//** === Variables de Tiempo == */
 const task = []; // Tareas agregadas =
 let time = 0;
-const timer = null; // Time of init =
-const timeBreak = null; // Time of break => Descanso =
-const current = null; // Tiempo Transcurriendo para trabajo =
+let timer = null; // Time of init =
+let timeBreak = null; // Time of break => Descanso =
+let current = null; // Tiempo Transcurriendo para trabajo =
+
+//** === Events Formulary === */
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //console.log("form");
+  idTask !== " " ? createTask(idTask.value) : (idTask.value = ` `);
+});
+
+//** === Function */
+const createTask = () => {};
